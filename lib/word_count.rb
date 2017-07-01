9d9bfc9216a2c42d90458a5b9bea6aaf07143910
Betty Mutai <bettymutai00@gmail.com>
2017-07-01 19:00:58 +0300

    added config.ru

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,3 +1,9 @@
 class String
-  define_method(:word_count) do
-end
+  define_method(:word_count) do |sentence,scope|
+    if scope=="Local"
+      (sentence.downcase.split(" ").select { |i| i.include?self.downcase}).length
+    else
+     sentence.downcase.scan(/(#{self.downcase})/).count
+     end
+   end
+ end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -2,4 +2,11 @@ require('rspec')
   require('word_count')
 
   describe('word_count')do
+    it("should count how many times a word appears") do
+      expect("peck".word_count("If Peter Piper picked a peck of pickled peppers","local")).to(eq(1))
+    end
+    it("will count how # of appearances of the given word including partials") do
+      expect("peck".word_count("If Peter Piper picked a peck of pickled peppers, where’s the peck of pickled peppers Peter Piper picked?” ","local")).to(eq(2))
+  end
+
   end
