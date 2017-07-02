153fe3b714e4343aed151bcc041326cc97164766
Betty Mutai <bettymutai00@gmail.com>
2017-07-02 21:26:51 +0300

    Added a form for users to enter a sentence and a word

--- app.rb
+++ app.rb
@@ -1,4 +1,25 @@
 require('sinatra')
+  require('sinatra/reloader')
+
 get('/') do
-  "Hello Friend!"
+  "<!DOCTYPE html>
+  <html>
+    <head>
+      <title>Word Count</title>
+       <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
+    </head>
+    <body>
+      <h1>Word Count</h1>
+      <h3>This is a website that uses a method to return how frequently a word appears in a given string</h3>
+      <div class="form-group">
+      <label for="word">Your word</label>
+      <input id="word" name="ord" class="form-control" type="text">
+    </div>
+    <div class="form-group">
+      <label for="sentence">Your sentence</label>
+      <input id="sentence" name="sentence" class="form-control" type="text">
+    </div>
+    <button type="submit" class="btn">Go!</button>
+    </body>
+  "
 end
