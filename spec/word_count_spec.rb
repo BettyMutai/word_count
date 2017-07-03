c7ed552070ba0174dba1b457d706162925206d07
Betty Mutai <bettymutai00@gmail.com>
2017-07-03 11:06:25 +0300

    Passed one argument to return frequency of word in a sentence

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -3,10 +3,6 @@ require('rspec')
 
   describe('word_count')do
     it("should count how many times a word appears") do
-      expect("peck".word_count("If Peter Piper picked a peck of pickled peppers","local")).to(eq(1))
+      expect("peck".word_count("If Peter Piper picked a peck of pickled peppers, where’s the peck of pickled peppers Peter Piper picked?")).to(eq(2))
     end
-    it("will count how # of appearances of the given word including partials") do
-      expect("peck".word_count("If Peter Piper picked a peck of pickled peppers, where’s the peck of pickled peppers Peter Piper picked?” ","local")).to(eq(2))
-  end
-
   end
