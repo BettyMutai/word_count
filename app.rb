f148ce301c616e342eceb28ade659c34159d5046
Betty Mutai <bettymutai00@gmail.com>
2017-07-03 11:03:26 +0300

    Added an output for my results to show number of words counted

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require('sinatra')
   require('sinatra/reloader')
+  require('./lib/word_count')
+  also_reload('lib/**/*.rb')
 
 get('/') do
   erb(:form)
+end
+
+get('/results') do
+  word = params.fetch('word')
+  sentence = params.fetch('sentence')
+  @totalcount = sentence.word_count(word)
+  erb(:results)
 end
