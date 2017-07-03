0b0933f80d7379a5aa8bc42a5992a28fc2da7ba3
Betty Mutai <bettymutai00@gmail.com>
2017-07-03 08:02:02 +0300

    Dried up my code

--- app.rb
+++ app.rb
@@ -2,24 +2,5 @@ require('sinatra')
   require('sinatra/reloader')
 
 get('/') do
-  "<!DOCTYPE html>
-  <html>
-    <head>
-      <title>Word Count</title>
-       <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
-    </head>
-    <body>
-      <h1>Word Count</h1>
-      <h3>This is a website that uses a method to return how frequently a word appears in a given string</h3>
-      <div class="form-group">
-      <label for="word">Your word</label>
-      <input id="word" name="ord" class="form-control" type="text">
-    </div>
-    <div class="form-group">
-      <label for="sentence">Your sentence</label>
-      <input id="sentence" name="sentence" class="form-control" type="text">
-    </div>
-    <button type="submit" class="btn">Go!</button>
-    </body>
-  "
+  erb(:form)
 end
